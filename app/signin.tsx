803f84b408c02dee3898f6adcab200bb7bd5cfbf
glconde <georgelouie.conde@edu.sait.ca>
2025-03-12 11:21:34 -0600

    login ui

--- app/signin.tsx
+++ app/signin.tsx
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Template from '../components/Template'
+import { StatusBar } from "expo-status-bar";
+import { TextInput } from "react-native";
+import ClimButton from "../components/ClimButton";
+import Template from "../components/Template";
+import styles from "../styles/styles";
 
-const SignIn = () => {
-    return(
-        <Template title="Sign in to ClimApp">
-        <Text>Welcome to ClimApp</Text>
-        </Template>
-    )
-}
+const handleLogin = () => {};
 
-export default SignIn;
+const Login = () => {
+  return (
+    <Template title="Login" vCentered={true}>
+      <TextInput
+        placeholder="Email Address or Username"
+        style={styles.defaultInput}
+      ></TextInput>
+      <TextInput
+        placeholder="Password"
+        style={styles.defaultInput}
+        secureTextEntry
+      ></TextInput>
+      <ClimButton title="LOGIN" color="magenta" onClick={handleLogin} />
+    </Template>
+  );
+};
+
+export default Login;
--- components/Template.tsx
+++ components/Template.tsx
@@ -1,17 +1,27 @@
-import {ScrollView, View, Text} from 'react-native'
-import styles from '../styles/styles'
+import { ScrollView, View, Text } from "react-native";
+import styles from "../styles/styles";
 
 type TemplateProps = {
-    children:any;
-    title?:string;
-}
-const Template = ({children, title=""}:TemplateProps) => {
-    return(
-        <ScrollView style={styles.mainView}>
-            <View style={styles.titleWrapper}><Text style={styles.title}>{title}</Text></View>
-            {children}
-        </ScrollView>
-    )
-}
+  children: any;
+  title?: string;
+  vCentered?: boolean;
+};
+const Template = ({
+  children,
+  title = "",
+  vCentered = false,
+}: TemplateProps) => {
+  return (
+    <ScrollView
+      style={styles.mainView}
+      contentContainerStyle={vCentered ? styles.mainViewVCentered : undefined}
+    >
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+      {children}
+    </ScrollView>
+  );
+};
 
-export default Template
+export default Template;
